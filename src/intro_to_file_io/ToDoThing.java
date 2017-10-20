f39ba8823a20ab3f888893cf02de7c8c6abfe203
DanielR1 <league@WorthyMini-5.attlocal.net>
2017-10-19 20:02:15 -0700

    br length

--- src/intro_to_file_io/ToDoThing.java
+++ src/intro_to_file_io/ToDoThing.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
 
 public class ToDoThing implements ActionListener {
 	
@@ -18,6 +21,7 @@ public class ToDoThing implements ActionListener {
 	JButton button2 = new JButton();
 	JButton button3 = new JButton();
 	JButton button4 = new JButton();
+	JLabel label = new JLabel();
 	ArrayList<String> list = new ArrayList<String>();
 	
 	public void setup() {
@@ -36,7 +40,9 @@ public class ToDoThing implements ActionListener {
 		button2.setText("Remove Task");
 		button3.setText("Save Text");
 		button4.setText("Load Task");
-		frame.pack();
+		label.setText("<html>");
+		panel.add(label);
+		frame.setSize(500, 900);
 	}
 public static void main(String[] args) {
 	
@@ -49,11 +55,20 @@ public void actionPerformed(ActionEvent e) {
 	if(e.getSource().equals(button1)) {
 	String task = JOptionPane.showInputDialog("Enter a task to add");
 	list.add(task);
+	String temp = "";
+	temp=label.getText();
+	temp+="<br/>"+task;
+	label.setText(temp);
+	
 }
 	if(e.getSource().equals(button2)) {
 		String r= JOptionPane.showInputDialog("Enter the task number you would like to remove");
 		int re = Integer.parseInt(r);
+		String v =list.get(re);
 	list.remove(re);
+	String temp = label.getText();
+	String oof = temp.substring(0, label.getText().length()-v.length());
+	label.setText(oof);
 	}
 }
 }
