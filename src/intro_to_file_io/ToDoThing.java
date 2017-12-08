9193dc3cfe32d7fb828c504d2473fe72eaf7c70d
DanielR1 <league@WorthyMini-5.attlocal.net>
2017-12-07 19:47:54 -0800

    Finished!!
    Finally

--- src/intro_to_file_io/ToDoThing.java
+++ src/intro_to_file_io/ToDoThing.java
@@ -3,12 +3,17 @@ package intro_to_file_io;
 import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -25,6 +30,8 @@ public class ToDoThing implements ActionListener {
 	JButton button3 = new JButton();
 	JButton button4 = new JButton();
 	JLabel label = new JLabel();
+	String filename;
+	JFileChooser choose;
 	ArrayList<String> list = new ArrayList<String>();
 
 	public void setup() {
@@ -46,6 +53,7 @@ public class ToDoThing implements ActionListener {
 		label.setText("<html>");
 		panel.add(label);
 		frame.setSize(500, 900);
+		lastFile();
 	}
 
 	public static void main(String[] args) {
@@ -68,7 +76,7 @@ public class ToDoThing implements ActionListener {
 		}
 		if (e.getSource().equals(button2)) {
 			String r = JOptionPane.showInputDialog("Enter the task number you would like to remove");
-			int re = Integer.parseInt(r);
+			int re = Integer.parseInt(r)-1;
 
 			String v = list.get(re);
 			list.remove(re);
@@ -81,19 +89,150 @@ public class ToDoThing implements ActionListener {
 			String oof = temp.replaceAll("<br/>" + v, "");
 			label.setText(oof);
 		}
-		int filecount = 0;
+
 		if (e.getSource().equals(button3)) {
-			filecount += 1;
+			 filename = JOptionPane.showInputDialog("Insert File Name");
 		
 			try {
-				FileWriter fw = new FileWriter("file" + filecount + ".txt");
+				FileWriter fw = new FileWriter(""+filename + ".txt");
 
-				fw.write(new String(Base64.getEncoder().encode(label.getText().getBytes())));
-
+				//fw.write(new String(Base64.getEncoder().encode(label.getText().getBytes())));
+				for (int i = 0; i < list.size(); i++) {
+					fw.write(""+list.get(i)+"\n");
+				}
 				fw.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
+			try {
+				FileWriter fw2 = new FileWriter("lastfile.txt");
+
+				//fw.write(new String(Base64.getEncoder().encode(label.getText().getBytes())));
+				fw2.write(filename);
+				fw2.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		if (e.getSource().equals(button4)) {
+			 choose=new JFileChooser();
+			 choose.showOpenDialog(frame);
+			 File chosen = choose.getSelectedFile();
+			 BufferedReader reader;
+			 try {
+				reader = new BufferedReader(new FileReader(chosen));
+				list.clear();
+				 String line = reader.readLine();
+				 String temp = "";
+				 list.add(line);
+				 int i=0;
+				 String temp2 = "";
+				 
+					temp2 = label.getText();
+					temp2 += "<br/>" + line;
+					label.setText(temp2);
+				 while(temp!=null) {
+					 i++;
+					
+						System.out.println(line);
+						
+						temp=reader.readLine();
+					
+						if(temp!=null) {
+							list.add(temp);
+							
+							temp2 = label.getText();
+							temp2 += "<br/>" + temp;
+							label.setText(temp2);
+						line +=temp;}
+					}
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		
+			try {
+				FileWriter fw = new FileWriter(""+filename + ".txt");
+			
+
+				//fw.write(new String(Base64.getEncoder().encode(label.getText().getBytes())));
+				for (int i = 0; i < list.size(); i++) {
+					fw.write(""+list.get(i)+"\n");
+				}
+				fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+	public void lastFile() {
+		 
+		 
+		 File file1 = new File("lastfile.txt");
+		 BufferedReader reader2;
+		 BufferedReader reader;
+		 
+		 String f;
+		try {
+			reader2 = new BufferedReader(new FileReader(file1));
+			f = reader2.readLine();
+			 File file2 = new File(""+f+".txt");
+			 try {
+				reader = new BufferedReader(new FileReader(file2));
+				list.clear();
+				 String line = reader.readLine();
+				 String temp = "";
+				 list.add(line);
+				 int i=0;
+				 String temp2 = "";
+				 
+					temp2 = label.getText();
+					temp2 += "<br/>" + line;
+					label.setText(temp2);
+				 while(temp!=null) {
+					 i++;
+					
+						System.out.println(line);
+						
+						temp=reader.readLine();
+					
+						if(temp!=null) {
+							list.add(temp);
+							
+							temp2 = label.getText();
+							temp2 += "<br/>" + temp;
+							label.setText(temp2);
+						line +=temp;}
+					}
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+		try {
+			FileWriter fw = new FileWriter(""+filename + ".txt");
+
+			//fw.write(new String(Base64.getEncoder().encode(label.getText().getBytes())));
+			for (int i = 0; i < list.size(); i++) {
+				fw.write(""+list.get(i)+"\n");
+			}
+			fw.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
 	}
 }
